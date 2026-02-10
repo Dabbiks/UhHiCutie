fdd74780db9a7422d49a33848e1e3a65e656b7b9
dabbiks <mikolajmarcingrotek@gmail.com>
2026-02-10 19:53:41 +0100

    Converter

---
+++ src/main/java/dabbiks/uhc/game/gameplay/items/conversion/ConversionManager.java
@@ -0,0 +1,48 @@
+package dabbiks.uhc.game.gameplay.items.conversion;
+
+import dabbiks.uhc.game.gameplay.items.recipes.loader.RecipeManager;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConversionManager implements Listener {
+
+    private final ItemConverter itemConverter;
+
+    public ConversionManager(RecipeManager recipeManager) {
+        this.itemConverter = new ItemConverter(recipeManager);
+    }
+
+    @EventHandler
+    public void onPickup(EntityPickupItemEvent event) {
+        if (!(event.getEntity() instanceof Player)) return;
+        ItemStack item = event.getItem().getItemStack();
+
+        ItemStack convertedItem = itemConverter.convert(item);
+        if (item.equals(convertedItem)) return;
+        event.getItem().setItemStack(convertedItem);
+    }
+
+    @EventHandler
+    public void onInventoryOpen(InventoryOpenEvent event) {
+        Inventory inventory = event.getInventory();
+        for (int i = 0; i < inventory.getSize(); i++) {
+            ItemStack item = inventory.getItem(i);
+            ItemStack converted = itemConverter.convert(item);
+
+            if (item != null && !item.equals(converted)) {
+                inventory.setItem(i, converted);
+            }
+        }
+    }
+
+
+}
---
+++ src/main/java/dabbiks/uhc/game/gameplay/items/conversion/ItemConverter.java
@@ -0,0 +1,47 @@
+package dabbiks.uhc.game.gameplay.items.conversion;
+
+import dabbiks.uhc.game.gameplay.items.ItemBuilder;
+import dabbiks.uhc.game.gameplay.items.ItemInstance;
+import dabbiks.uhc.game.gameplay.items.data.enchants.EnchantCalculator;
+import dabbiks.uhc.game.gameplay.items.recipes.data.RecipeInstance;
+import dabbiks.uhc.game.gameplay.items.recipes.loader.RecipeManager;
+import de.tr7zw.nbtapi.NBTItem;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Optional;
+
+public class ItemConverter {
+
+    private final RecipeManager recipeManager;
+
+    public ItemConverter(RecipeManager recipeManager) {
+        this.recipeManager = recipeManager;
+    }
+
+    public ItemStack convert(ItemStack item) {
+        if (item == null || item.getType() == Material.AIR) return item;
+        if (!item.hasItemMeta()) return item;
+
+        NBTItem nbtItem = new NBTItem(item);
+        if (!nbtItem.hasKey("UHC_ITEM")) return item;
+
+        String recipeId = item.getType().name().toLowerCase();
+        Optional<RecipeInstance> optional = recipeManager.getRecipeById(recipeId);
+
+        if (optional.isEmpty()) return item;
+
+        RecipeInstance recipe = optional.get();
+        ItemInstance itemInstance = recipe.getResult().clone();
+
+        ItemMeta meta = item.getItemMeta();
+        int power = EnchantCalculator.convertVanillaToPower(meta);
+
+        if (power > 1) {
+            itemInstance.setEnchants(EnchantCalculator.calculateEnchants(power, itemInstance.getEnchantSlot()));
+        }
+
+        return new ItemBuilder(itemInstance).build();
+    }
+}
