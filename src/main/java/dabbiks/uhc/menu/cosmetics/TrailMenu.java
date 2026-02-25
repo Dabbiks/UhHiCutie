fdb57ab09ad13151ba9d97e21b103b08ec2e09a6
Dabbiks <mikolajmarcingrotek@gmail.com>
2026-02-25 13:38:40 +0100

    Trail menu fix

--- src/main/java/dabbiks/uhc/menu/cosmetics/TrailMenu.java
+++ src/main/java/dabbiks/uhc/menu/cosmetics/TrailMenu.java
@@ -33,8 +33,11 @@ public class TrailMenu extends FastInv {
 
     private void render() {
         int[] slots = {
+                4,
                 10, 11, 12, 13, 14, 15, 16,
-                19, 20, 21, 22, 23, 24, 25
+                19, 20, 21, 22, 23, 24, 25,
+                28, 29, 30, 31, 32, 33, 34,
+                37, 38, 39, 40, 41, 42, 43
         };
 
         int index = 0;
