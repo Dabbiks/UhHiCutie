694cdd11d18fc0b709764e04ea71083b556e89aa
dabbiks <mikolajmarcingrotek@gmail.com>
2026-04-02 13:07:28 +0200

    Easter

--- src/main/java/dabbiks/uhc/lobby/easter/EasterEggManager.java
+++ src/main/java/dabbiks/uhc/lobby/easter/EasterEggManager.java
@@ -14,6 +14,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockGrowEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 
 import java.util.List;
 import java.util.Random;
@@ -56,7 +57,21 @@ public class EasterEggManager implements Listener {
     @EventHandler
     public void onInteract(PlayerInteractEvent event) {
         Block block = event.getClickedBlock();
-        if (block != null && block.getType() == Material.SNIFFER_EGG) {
+        if (block == null) return;
+
+        Player player = event.getPlayer();
+
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK && event.getHand() == EquipmentSlot.HAND) {
+            if (player.getInventory().getHeldItemSlot() == 8 && player.getInventory().getItemInMainHand().getType() == Material.BONE) {
+                Location newLoc = block.getRelative(event.getBlockFace()).getLocation();
+                EasterLocationData.addLocation(newLoc);
+                player.sendMessage("§aPomyślnie dodano nową lokację jajka wielkanocnego!");
+                event.setCancelled(true);
+                return;
+            }
+        }
+
+        if (block.getType() == Material.SNIFFER_EGG) {
             if (event.getAction() == Action.PHYSICAL) {
                 event.setCancelled(true);
                 return;
@@ -64,7 +79,6 @@ public class EasterEggManager implements Listener {
 
             if (block.getWorld().getName().equals("world")) {
                 event.setCancelled(true);
-                Player player = event.getPlayer();
 
                 block.setType(Material.AIR);
                 block.getWorld().spawnParticle(Particle.CLOUD, block.getLocation().add(0.5, 0.5, 0.5), 20, 0.3, 0.3, 0.3, 0.05);
@@ -73,6 +87,10 @@ public class EasterEggManager implements Listener {
                 PersistentData data = PersistentDataManager.getData(player.getUniqueId());
                 data.addStats(PersistentStats.POWDER, 100);
                 player.sendMessage("§f100 " + symbolU.SCOREBOARD_POWDER);
+
+                for (Player p : block.getWorld().getPlayers()) {
+                    p.sendMessage("§d" + player.getName() + " §fznalazł jajko wielkanocne!");
+                }
 
                 spawnRandomEgg();
             }
