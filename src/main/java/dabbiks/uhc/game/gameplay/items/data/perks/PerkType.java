9a112c2d0840b0210e13b9e3c53272e0578c54b8
dabbiks <mikolajmarcingrotek@gmail.com>
2026-04-06 22:35:40 +0200

    Flare perk

--- src/main/java/dabbiks/uhc/game/gameplay/items/data/perks/PerkType.java
+++ src/main/java/dabbiks/uhc/game/gameplay/items/data/perks/PerkType.java
@@ -9,9 +9,10 @@ public enum PerkType {
     RABBIT_JUMP("Króliczy wyskok", List.of("§7Przytrzymywanie " + symbolU.SHIFT + " §7ładuje buty.", "§7Po puszczeniu przycisku §ewyrzucają do góry.")),
     COAL_FUELED("Fabrykator CO₂", List.of("§7Przedmiot §espala węgiel§7, żeby", "§7wzmocnić swoje statystyki.")),
     CONTACT_EXPLOSION("Eksplozja przy kontakcie", List.of("§7Przedmiot wywoła wybuch, gdy po", "§7raz pierwszy napotka blok.")),
-    ABSORPTION_ABSORBER("Absorber Absorpcji", List.of("§7Przy otrzymywaniu obrażeń wysysa absorpcję przeciwnika", "i regeneruje posiadacza o połowę jej wartości")),
+    ABSORPTION_ABSORBER("Absorber Absorpcji", List.of("§7Przy otrzymywaniu obrażeń wysysa absorpcję przeciwnika", "§7i regeneruje posiadacza o połowę jej wartości")),
     BURNING_ATTACK("Płonący atak", List.of("§7Po spożyciu ataki podpalają graczy przez 30 sekund")),
-    UPGRADE_CRYSTAL("Wzmocnienie zaklęć", List.of("§7Po użyciu inkrementuje poziom wszystkich enchantów", "§7na całej zbroi używającego"));
+    UPGRADE_CRYSTAL("Wzmocnienie zaklęć", List.of("§7Po użyciu inkrementuje poziom wszystkich enchantów", "§7na całej zbroi używającego")),
+    PLAYER_RADAR("Wykrywanie graczy", List.of("§7Po odpaleniu podaje dystans do najbliższego", "§7gracza w promieniu 200 kratek"));
 
     private final String name;
     private final List<String> lore;
